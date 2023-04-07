61d77830053e205777b1a05a47fde241a50575da
Subhaneet Shrestha <shresthasubhaneet@gmail.com>
2023-04-08 01:36:01 +0545

    added register component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import { Nunito } from 'next/font/google'
+import { Nunito } from 'next/font/google';
 
-import './globals.css'
-import Navbar from './components/navbar/Navbar'
+import './globals.css';
+import Navbar from './components/navbar/Navbar';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata = {
   title: 'AirBnB Clone',
   description: 'This is a AirBnB clone made with next and tailwindcss',
-}
+};
 
 const font = Nunito({
-  subsets: ['latin']
-})
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
